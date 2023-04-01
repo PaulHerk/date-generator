17dc3ee4eb79fd92f9c0959ee587fc943f0dc3f5
Kaenigsion <herklotzpaul@gmail.com>
2023-04-01 11:48:21 +0200

    fudged up with git somehow again...

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,24 +1,32 @@
+use apd::random_date;
 use clap::Parser;
-use rand::prelude::*;
 use regex::Regex;
 use std::fs;
-use time::{format_description, OffsetDateTime};
 
-/// Simple program to greet a person
+/// Simple program to append dates to your sources
 #[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
+#[command(author, version, author, about, long_about = None)]
 struct Args {
     /// Name of the input file
     #[arg(short, long)]
     input: String,
 
-    /// Start date
-    #[arg(short, long)]
+    /// Name of the output file
+    #[arg(default_value_t = format!("sources.txt"))]
+    output: String,
+
+    /// Format: YYYY-MM-DD_HH
+    /// Or: today + N, example: -e -2 is yesterday
+    #[arg(short, long, allow_hyphen_values = true)]
     start_date: String,
 
-    /// End date
-    #[arg(short, long)]
+    /// Same as START_DATE
+    #[arg(short, long, default_value_t = format!("0"), allow_hyphen_values = true)]
     end_date: String,
+
+    /// Output formatting ([day]-[month]-[year] [hour])
+    #[arg(short, long, default_value_t = format!("[day].[month].[year] [hour]:00"))]
+    out_format: String,
 }
 
 fn main() {
@@ -29,23 +37,13 @@ fn main() {
     let path = args.input;
     let file_content = fs::read_to_string(path).unwrap().trim().to_string();
 
-    for cap in regex.captures_iter(&file_content) {
-        dbg!(cap);
+    let mut new_file_content = file_content.clone();
+    for cap in regex.find_iter(&file_content) {
+        let rand_date = random_date(&args.start_date, &args.end_date, &args.out_format);
+        let insert_string = &format!(" {}", rand_date);
+        new_file_content.insert_str(cap.end(), insert_string)
     }
 
-    let format = format_description::parse(
-        "[year]-[month]-[day]_[hour]:[minute]:[second] [offset_hour \
-         sign:mandatory]:[offset_minute]:[offset_second]",
-    )
-    .unwrap();
-    let start_date = OffsetDateTime::parse(&format!("{} +00:00:00", args.start_date), &format)
-        .unwrap()
-        .unix_timestamp();
-    let end_date = OffsetDateTime::parse(&format!("{} +00:00:00", args.end_date), &format)
-        .unwrap()
-        .unix_timestamp();
-
-    let random_unix = rand::thread_rng().gen_range(start_date..=end_date);
-    let random_date = OffsetDateTime::from_unix_timestamp(random_unix).unwrap();
-    println!("{}", random_date)
+    fs::write(args.output, &new_file_content)
+        .unwrap_or_else(|_| panic!("Failed to write file, output: \n {}", new_file_content));
 }
